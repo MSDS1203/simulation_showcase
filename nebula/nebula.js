d4d6a1f8df61a850e866b9ae2289ff89ef3356db
srazam <srazam5@gmail.com>
2025-04-29 12:41:58 -0400

    Adding post processing and arranging lighting

--- nebula/nebula.js
+++ nebula/nebula.js
@@ -1,9 +1,11 @@
 import * as THREE from 'three';
+import * as POSTPROCESSING from 'postprocessing';
 
 let app = {
   el: document.getElementById("app"),
   scene: null,
   renderer: null,
+  composer: null,
   camera: null
 }
 
@@ -11,7 +13,6 @@ const cloudParticles = [];
 
 const init = () => {
     app.renderer = new THREE.WebGLRenderer();
-    console.log(app.renderer);
     app.renderer.setSize ( window.innerWidth, window.innerHeight);
     app.el.appendChild (app.renderer.domElement);
 
@@ -22,41 +23,51 @@ const init = () => {
     app.camera.rotation.y = -0.12;
     app.camera.rotation.z = 0.27;
 
-    app.scene.fog = new THREE.FogExp2(0x03544e, 0.0001);
-    app.renderer.setClearColor(app.scene.fog.color);
-    
-    const ambient = new THREE.AmbientLight(0x555555, 0.75);
+    const ambient = new THREE.AmbientLight(0x555555);
     app.scene.add(ambient);
 
-    const directionalLight = new THREE.DirectionalLight(0xff8c19);
-    directionalLight.position.set(0.1, 0, 0);
-    app.scene.add(directionalLight);
-
-    const orangeLight = new THREE.PointLight(0xcc6600, 200, 450, 1.7);
-    orangeLight.position.set(200, 300, 100);
+    const orangeLight = new THREE.PointLight(0xcc6600, 700000);
+    orangeLight.position.set(-200, 300, -100);
     app.scene.add(orangeLight);
 
-    const redLight = new THREE.PointLight(0xd8547e, 50, 450, 1.7);
-    redLight.position.set(200, 300, 100);
+    const redLight = new THREE.PointLight(0xd8547e, 700000);
+    redLight.position.set(0, 300, -100);
     app.scene.add(redLight);
 
-    const blueLight = new THREE.PointLight(0x3677ac, 50, 450, 1.7);
-    blueLight.position.set(200, 300, -100);
+    const blueLight = new THREE.PointLight(0x3677ac, 700000);
+    blueLight.position.set(500, 300, 0);
     app.scene.add(blueLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xff8c19);
+    directionalLight.position.set(0, 0, 1);
+    app.scene.add(directionalLight);
+
+    app.scene.fog = new THREE.FogExp2(0x03544e, 0.001);
+    app.renderer.setClearColor(app.scene.fog.color);
     
     const textureLoader = new THREE.TextureLoader();
+
+    const starTexture = textureLoader.load("stars.jpg");
+    const starEffect = new POSTPROCESSING.TextureEffect({
+        blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
+        texture: starTexture,
+    });
+    starEffect.blendMode.opacity.value = 0.2;
+
     const cloudTexture = textureLoader.load("smoke.png");
     const cloudGeo = new THREE.PlaneGeometry(500, 500);
-    const cloudMaterial = new THREE.MeshPhongMaterial({
+    const cloudMaterial = new THREE.MeshLambertMaterial({
         map: cloudTexture, 
-        transparent: true});
+        transparent: true,
+        emissive: 0x222222, 
+        emissiveIntensity: 0.5,});
 
-    for (let p = 0; p < 50; p++){
-        let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+    for (let p = 0; p < 70; p++){
+        const cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
         cloud.position.set(
             Math.random() * 800 - 400,
             500,
-            Math.random() * 500 - 500
+            Math.random() * 800 - 400
         );
 
         cloud.rotation.x = 1.16;
@@ -66,14 +77,30 @@ const init = () => {
         cloudParticles.push(cloud);
         app.scene.add(cloud);
     }
+
+    const bloomEffect = new POSTPROCESSING.BloomEffect({
+        blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
+        kernelSize: POSTPROCESSING.KernelSize.SMALL,
+        useLuminanceFilter: true,
+        luminanceThreshold: 0.3,
+        luminanceSmoothing: 0.75
+    });
+    bloomEffect.blendMode.opacity.value = 1.5;
+
+    const effectPass = new POSTPROCESSING.EffectPass(app.camera, bloomEffect, starEffect);
+    effectPass.renderToScreen = true;
+
+    app.composer = new POSTPROCESSING.EffectComposer(app.renderer);
+    app.composer.addPass(new POSTPROCESSING.RenderPass(app.scene, app.camera));
+    app.composer.addPass(effectPass);
 };
 
 const render = () => {
   cloudParticles.forEach(p =>{
-    p.rotation.z -= 0.001;
+    p.rotation.z -= 0.002;
   });
+  app.composer.render(0.1);
   requestAnimationFrame(render);
-  app.renderer.render(app.scene, app.camera);
 };
 
 init();
